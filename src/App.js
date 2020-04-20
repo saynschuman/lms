1de84befad7c0466c555b5dc5344091c6c7d766f
saynschuman <saynschuman@gmail.com>
2020-04-20 18:50:07 +0300

    auth removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import { authAPI } from './features/auth/api';
 
-function App() {
-  // React.useEffect(() => {
-  //   authAPI.authenticateUser('document', '123');
-  // }, []);
-  const redirect = () => window.location.replace('/client');
-  return <button onClick={redirect}>hello</button>;
-}
+const App = () => {
+  return <div>client</div>;
+};
 
 export default App;
